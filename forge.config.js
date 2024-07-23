20ebe91037de1d8820c1d727c74a297673f1119a
Ab Aeterno <alitomosca@gmail.com>
2024-07-23 17:30:40 -0300

    Electron forge packaging setup

---
+++ forge.config.js
@@ -0,0 +1,62 @@
+const { FusesPlugin } = require("@electron-forge/plugin-fuses");
+const { FuseV1Options, FuseVersion } = require("@electron/fuses");
+
+module.exports = {
+  packagerConfig: {
+    asar: true,
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: "@electron-forge/maker-squirrel",
+      config: {},
+    },
+    {
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
+    },
+    {
+      name: "@electron-forge/maker-deb",
+      config: {},
+    },
+    {
+      name: "@electron-forge/maker-rpm",
+      config: {},
+    },
+  ],
+  plugins: [
+    {
+      name: "@electron-forge/plugin-auto-unpack-natives",
+      config: {},
+    },
+    // Fuses are used to enable/disable various Electron functionality
+    // at package time, before code signing the application
+    new FusesPlugin({
+      version: FuseVersion.V1,
+      [FuseV1Options.RunAsNode]: false,
+      [FuseV1Options.EnableCookieEncryption]: true,
+      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+      [FuseV1Options.EnableNodeCliInspectArguments]: false,
+      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+      [FuseV1Options.OnlyLoadAppFromAsar]: true,
+    }),
+  ],
+  hooks: {
+    postMake: async (forgeConfig, opts) => {
+      // Copy assets and make trees dir in output
+      console.log(forgeConfig, opts);
+      const fs = require("fs");
+      fs.cp(
+        "assets/",
+        "out/isaac-skilltreegen-win32-x64/assets/",
+        { recursive: true },
+        (err) => {
+          if (err) console.warn(err);
+        }
+      );
+      fs.mkdir("out/isaac-skilltreegen-win32-x64/trees/", (err) => {
+        if (err) console.warn(err);
+      });
+    },
+  },
+};
